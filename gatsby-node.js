1987f4fdd1091efd00ad63dec9c7d57c40c74674
Saad Ahmad Malik <saadislion@gmail.com>
2024-11-23 18:43:14 +0500

    Initial commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,51 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
+const path = require("path");
 
-/**
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
-}
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+
+  // Add a slug field to MarkdownRemark nodes
+  if (node.internal.type === "MarkdownRemark") {
+    const fileNode = getNode(node.parent); // Get the parent file node
+    const slug = path.basename(fileNode.relativePath, ".md"); // Generate slug from file name
+
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug, // Add the slug field
+    });
+  }
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  // Query for all MarkdownRemark nodes and their slugs
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    throw result.errors; // Throw errors if the query fails
+  }
+
+  // Generate pages for each Markdown file
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: `/blog/${node.fields.slug}`, // Use the slug in the URL
+      component: path.resolve("./src/templates/blog-post.js"),
+      context: {
+        slug: node.fields.slug, // Pass the slug to the template
+      },
+    });
+  });
+};
